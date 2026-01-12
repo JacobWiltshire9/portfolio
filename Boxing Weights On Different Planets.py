76c9c08981dccc9b7cd1ff7f26e16c32b87b8d15
Jacob Wiltshire <jacobrw18@gmail.com>
2026-01-12 16:54:28 -0700

    Codecademy Project
    This Python program asks the user for their weight and lets them choose a planet. It then calculates and displays what the user would weigh on that planet by applying the planet’s gravity multiplier and printing the result.

---
+++ Boxing Weights On Different Planets.py
@@ -0,0 +1,44 @@
+# Boxing Weights On Different Planets
+print("I have information for the following planets:\n")
+
+# Planets in order excludng Earth
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+# User enters how much they weigh
+weight = input("Enter your weight: ")
+
+# Weight is converted to float so it can be calculated
+weight = float(weight)
+
+# User inputs which planet they would prefer to fight on and have their weight converted
+planet = input("Select the number of the planet you woud like to fight on: ")
+
+
+
+# If statements to calculate his weight on the chosen planet and declare what planet will be printed
+
+if planet == "1":
+  weight = weight * .91
+  planet = "Venus"
+elif planet == "2":
+  weight = weight * .38
+  planet = "Mars"
+elif planet == "3":
+  weight = weight * 2.34
+  planet = "Jupiter"
+elif planet == "4":
+  weight = weight * 1.06
+  planet = "Saturn"
+elif planet == "5":
+  weight = weight * .92
+  planet = "Uranus"
+elif planet == "6":
+  weight = weight * 1.19
+  planet = "Neptune"
+
+# This round weight to two decimal points
+weight = round(weight,1)
+
+# The statement will convert the int planet and the float weight to strings and print the given information
+print("Your weight on " + str(planet) + " is: " + str(weight))
